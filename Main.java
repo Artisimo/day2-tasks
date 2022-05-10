16085dfba5dff68d614841908e7c0658f1e0011f
Artis Birznieks <birznieks.artis@gmail.com>
2022-05-10 13:33:29 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.lang.*;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        byte byte1 = 127;
+        short short1 = 32000;
+        int int1 = 2000000000;
+        long long1 = 12123144;
+        float float1 = 1.123f;
+        double double1 = 1.1234;
+        boolean isTrue = true;
+        char c = 'c';
+
+        System.out.println("Byte: " + byte1 + "\n" +
+                            "Short: " + short1 + "\n" +
+                            "Int: " + int1  + "\n" +
+                            "Long: " + long1 + "\n" +
+                            "Float: " + float1 + "\n" +
+                            "Double: " + double1 + "\n" +
+                            "Boolean: " + isTrue + "\n" +
+                            "Char: " + c);
+    }
+}
