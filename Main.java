d07bb0b71e5d6276ecbaa6c97c55669f29a48630
Artis Birznieks <birznieks.artis@gmail.com>
2022-05-10 13:44:11 +0300

    Created a method that compares two numbers

--- Main.java
+++ Main.java
@@ -2,23 +2,42 @@ import java.lang.*;
 import java.util.*;
 public class Main {
 
-    public static void main(String[] args) {
-        byte byte1 = 127;
-        short short1 = 32000;
-        int int1 = 2000000000;
-        long long1 = 12123144;
-        float float1 = 1.123f;
-        double double1 = 1.1234;
-        boolean isTrue = true;
-        char c = 'c';
+    static void compare(int a, int b){
+        boolean isEqual = false;
+        boolean isLessThan = false;
+        boolean isLessOrEqual = false;
+        boolean isGreater = false;
+        boolean isGreaterOrEqual = false;
 
-        System.out.println("Byte: " + byte1 + "\n" +
-                            "Short: " + short1 + "\n" +
-                            "Int: " + int1  + "\n" +
-                            "Long: " + long1 + "\n" +
-                            "Float: " + float1 + "\n" +
-                            "Double: " + double1 + "\n" +
-                            "Boolean: " + isTrue + "\n" +
-                            "Char: " + c);
+        if(a == b){
+            isEqual = true;
+        }
+
+        if(a < b){
+            isLessThan = true;
+        }
+
+        if(a <= b){
+            isLessOrEqual = true;
+        }
+
+        if(a > b){
+            isGreater = true;
+        }
+
+        if(a >= b){
+            isGreaterOrEqual = true;
+        }
+
+        System.out.println("Is " + a + " equal to " + b + "? - " + isEqual);
+        System.out.println("Is " + a + " less than " + b + "? - " + isLessThan);
+        System.out.println("Is " + a + " less or equal to " + b + "? - " + isLessOrEqual);
+        System.out.println("Is " + a + " greater than " + b + "? - " + isGreater);
+        System.out.println("Is " + a + " greater or equal to " + b + "? - " + isGreaterOrEqual);
+    }
+
+    public static void main(String[] args) {
+
+        compare(10,10);
     }
 }
